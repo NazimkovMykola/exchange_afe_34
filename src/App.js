71482453bc926e54df40fe21de1a235b9ac29931
Mykola <148981382+NazimkovMykola@users.noreply.github.com>
2024-05-23 09:20:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
 
 function App() {
+const cur = new Freecurrencyapi('fca_live_qWTIYYyfmHFoFXh63B1MJAxOQxLF5pUkjIFQJZi4')
+
+const curency = useRef(null)
+const base = useRef(null)
+const val = useRef(null)
+const [result, setResult] = useState('')
+
+
+function getResult () {
+  const base_currency = base.current.value
+  const currencies = curency.current.value
+cur.latest({
+  base_currency,
+  currencies
+}).then(response => {
+  console.log(response.data[base_currency], val.current.value)
+  setResult(val.current.value * response.data[currencies])
+});
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={val} placeholder='Скільки міняємо'/>
+      <select ref={base}>
+        <option value='EUR'>Євро</option>
+        <option value='USD'>Доллар</option>
+        <option value='PLN'>Злотий</option>
+      </select>
+      <div>На що </div>
+      <select ref={curency}>
+        <option value='EUR'>Євро</option>
+        <option value='USD'>Доллар</option>
+        <option value='PLN'>Злотий</option>
+      </select>
+      <div onClick={getResult}>Сконвертувати</div>
+      <div>{result}</div>
     </div>
   );
 }
